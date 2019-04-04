c0779bbfbeccab94a3a4654acc9a3680eb489c15
Priyanka Gupta <pggnit213@gmail.com>
2019-04-04 12:05:53 +0530

    scroll top btn added fourth commit

---
+++ assets/js/myScript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+      $('#gotopbtn').fadeIn();
+    }  else{
+      $('#gotopbtn').fadeOut();
+    }
+  });
+
+  $('#gotopbtn').click(function(){
+    $('html,body').animate({scrollTop:40},800);
+  });
+});
